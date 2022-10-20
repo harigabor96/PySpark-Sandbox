88b96dfb6b17df6a8369294d9876767b914cb862
Hári Gábor <gabor.hari@ecobit.hu>
2022-10-20 20:54:15 +0200

    sandbox runs

--- pyspark-sandbox/etl/SandboxPipeline.py
+++ pyspark-sandbox/etl/SandboxPipeline.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.types import StructType, StructField, StringType
 
 
 class SandboxPipeline:
@@ -8,15 +9,37 @@ class SandboxPipeline:
         self.raw_zone_path = raw_zone_path
         self.curated_zone_path = curated_zone_path
 
+        self.inputPath = self.raw_zone_path + "/{*}"
+
+        self.inputSchema = StructType([
+            StructField("sandbox_field", StringType(), True)
+        ])
+
+        self.outputDatabaseName = "sandboxdb"
+        self.outputTableName = "sandboxtable"
+        self.outputDataRelativePath = f"{self.outputDatabaseName}.db/{self.outputTableName}/data"
+        self.outputCheckpointRelativePath = f"{self.outputDatabaseName}.db/{self.outputTableName}/checkpoint"
+
     def execute(self) -> None:
         self.__load(self.__transform(self.__extract()))
 
     def __extract(self) -> DataFrame:
-        print("")
+        return self.spark\
+            .readStream\
+            .option("sep", ";")\
+            .option("header", "true")\
+            .schema(self.inputSchema)\
+            .csv(self.inputPath)
 
     def __transform(self, extracted_df: DataFrame) -> DataFrame:
         return extracted_df
 
     def __load(self, transformed_df: DataFrame) -> None:
-        transformed_df
-
+        transformed_df\
+            .writeStream\
+            .trigger(availableNow=True)\
+            .outputMode("append")\
+            .format("console")\
+            .option("truncate", False)\
+            .start()\
+            .awaitTermination()
--- pyspark-sandbox/init/Router.py
+++ pyspark-sandbox/init/Router.py
@@ -1,3 +1,4 @@
+from etl.SandboxPipeline import SandboxPipeline
 from init.Conf import Conf
 from pyspark.sql import SparkSession
 
@@ -6,9 +7,7 @@ class Router:
 
     @staticmethod
     def execute_pipeline(spark: SparkSession, conf: Conf):
-        pipeline = conf.pipeline
-
-        if pipeline == "sandbox-pipeline":
-            print("sandbox pipeline!")
+        if conf.pipeline == "sandbox-pipeline":
+            SandboxPipeline(spark, conf.raw_zone_path, conf.curated_zone_path).execute()
         else:
             raise Exception("Pipeline is not registered in the router!")
