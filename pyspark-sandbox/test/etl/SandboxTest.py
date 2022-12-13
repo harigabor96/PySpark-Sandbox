70fad569974a384b568966f971540b6fa0475994
Hári Gábor <gabor.hari@ecobit.hu>
2022-12-13 16:57:07 +0100

    testing

--- pyspark-sandbox/test/etl/SandboxTest.py
+++ pyspark-sandbox/test/etl/SandboxTest.py
@@ -1,5 +1,9 @@
 import unittest
 
 class TestCase(unittest.TestCase):
+
     def test_something(self):
+        self.assertEqual(True, True)  # add assertion here
+
+    def test_something_else(self):
         self.assertEqual(True, True)  # add assertion here
