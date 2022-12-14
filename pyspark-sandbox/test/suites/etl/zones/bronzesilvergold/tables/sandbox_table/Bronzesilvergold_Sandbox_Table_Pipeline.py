48e38904ca3655cb8be994c0657a409a5cc73ac8
Hári Gábor <gabor.hari@ecobit.hu>
2022-12-14 14:05:46 +0100

    sample test updated

--- pyspark-sandbox/test/suites/etl/zones/bronzesilvergold/tables/sandbox_table/Bronzesilvergold_Sandbox_Table_Pipeline.py
+++ pyspark-sandbox/test/suites/etl/zones/bronzesilvergold/tables/sandbox_table/Bronzesilvergold_Sandbox_Table_Pipeline.py
@@ -2,8 +2,5 @@ from utils.SparkTest import SparkTest
 
 class Bronzesilvergold_Sandbox_Table_Pipeline(SparkTest):
 
-    def test_something(self):
-        self.assertEqual(True, True)
-
-    def test_something_else(self):
-        self.assertEqual(True, True)
+    def test_pipeline(self):
+        self.assertTrue(True)
